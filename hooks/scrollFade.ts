ed1ba4e2fe8c00a54c0e8c1721cd1353740a2fbe
oyarzunaspee <oyarzunaspee.work@gmail.com>
2026-05-05 05:23:22 -0400

    fix comparepages

--- hooks/scrollFade.ts
+++ hooks/scrollFade.ts
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import useDetectScroll from "@smakss/react-scroll-direction";
+import { useAppSelector } from "../store/hooks";
 
 export const useScrollFade = () => {
     const {scrollDir, scrollPosition} = useDetectScroll();
     const [scroll, setScroll] = useState<boolean>(false);
 
+    const comparePages = useAppSelector((state) => state.pageMenu.value.compare)
+
     useEffect(() => {
-    if (scrollDir == "up" || scrollPosition.bottom < window.innerHeight) {
-      setScroll(true);
-    } else if (scrollDir == "down" && scrollPosition.bottom > window.innerHeight) {
-      setScroll(false);
-    }
-  }, [scrollDir, scrollPosition.bottom]);
+      if (comparePages) {
+        setScroll(true)
+      } else if ((scrollDir == "up" || scrollPosition.bottom < window.innerHeight) && !comparePages) {
+        setScroll(true);
+      } else if ((scrollDir == "down" && scrollPosition.bottom > window.innerHeight) && !comparePages) {
+        setScroll(false);
+      }
+    }, [scrollDir, scrollPosition.bottom, comparePages]);
   
     return scroll;
   };
--- pages/@edition/chapter/@chapter/+Page.tsx
+++ pages/@edition/chapter/@chapter/+Page.tsx
@@ -1,11 +1,12 @@
 import { useAppSelector, useAppDispatch } from "../../../../store/hooks";
 import { usePageContext } from "vike-react/usePageContext";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import type { ComparisonResponse, PageData } from "../../../../store/types";
 import { useLazyGetComparePagesQuery } from "../../../../store/api/mangaApi";
 import { closeCompare, updateFetching, closeScreen } from "../../../../store/slices/pageMenu";
 import { setLastChapter } from "../../../../store/slices/maxNextChapter";
+import { ImgComparisonSlider } from '@img-comparison-slider/react';
 
 import type { customPageContext } from "../../../+onCreatePageContext";
 
@@ -14,6 +15,7 @@ import ProgressBar from "./sections/ProgressBar";
 
 import { useData } from "vike-react/useData";
 import type { Data } from "./+data";
+
 
 export default function Page() {
     const dispatch = useAppDispatch()
@@ -70,7 +72,7 @@ export default function Page() {
             <div className="bg-white flex flex-wrap">
                 {pages.map((page: PageData) => {
 
-                    if (!comparePages || !comparePagesList || (comparePages && comparePagesList && !page.content)) {
+                        if (!comparePages || !comparePagesList || (comparePages && comparePagesList && !page.content)) {
                         // for double pages
                         page.double ? i = -i : Math.abs(i)
                         const order = page.double ? page.number - i : page.number
@@ -83,10 +85,11 @@ export default function Page() {
                                 <img className="border-b-2 border-dotted border-link" src={page.link} />
                             </div>
                         )
-                    } else if (comparePages && page.content && (comparePagesList && comparePagesList.pages && page.content)) {
+                        } else if (comparePages && page.content && (comparePagesList && comparePagesList.pages && page.content)) {
                         j++
                         return (
                             <>
+
                                 <PageComparison key={`viewer-${page.number}`} page={page} comparePages={comparePagesList.pages} i={j} />
 
                             </>
@@ -100,21 +103,29 @@ export default function Page() {
 }
 
 const PageComparison = ({ page, comparePages, i }: { page: PageData, comparePages?: Partial<PageData>[], i: number }) => {
+    const first = useRef<HTMLImageElement | null>(null)
+    const second = useRef<HTMLImageElement | null>(null)
+    console.log(first)
+
+    useEffect(() => {
+        if (first.current && second.current) {
+            const max = Math.max(first.current.height, second.current.height)
+            first.current.style.height = `${max}px`
+            second.current.style.height = `${max}px`
+        }
+    }, [first.current, second.current])
 
 
     return (
 
-        <div style={{ order: page.number }} className="basis-full">
-            {/* <ReactCompareSlider
-                handle={<Handle buttonStyle={{
-                    backdropFilter: "blur(1px)",
-                    WebkitBackdropFilter: "blur(1px)",
-                    width: "2.5rem",
-                    height: "2.5rem"
-                }} />}
-                itemOne={<ReactCompareSliderImage onLoad={function UU() { }} src={page.link} alt="Image one" />}
-                itemTwo={<ReactCompareSliderImage onLoad={function UU() { }} src={comparePages ? comparePages[i - 1]?.link : ""} alt="Image two" />}
-            /> */}
+        <div style={{ order: page.number }} className="basis-full slider-wrapper">
+            <ImgComparisonSlider className="coloured-slider">
+                <img ref={first} slot="first" src={page.link} />
+                <img ref={second} slot="second" src={comparePages ? comparePages[i - 1]?.link : ""} />
+                <svg slot="handle" className="custom-animated-handle" xmlns="http://www.w3.org/2000/svg" width="100" viewBox="-8 -3 16 6">
+                    <path d="M -5 -2 L -7 0 L -5 2 M -5 -2 L -5 2 M 5 -2 L 7 0 L 5 2 M 5 -2 L 5 2" stroke-width="1" vector-effect="non-scaling-stroke"></path>
+                </svg>
+            </ImgComparisonSlider>
         </div>
     )
 }
