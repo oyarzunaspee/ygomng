a7db81b7a5f4724ff00bb7c3659a936dfff66a31
oyarzunaspee <oyarzunaspee.work@gmail.com>
2026-07-05 23:18:42 -0400

    remove vercel in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import react from "@vitejs/plugin-react";
 
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
-import { vercel } from 'vite-plugin-vercel/vite'
 
 
 export default defineConfig({
   plugins: [
     vike(),
-    vercel(),
 
     react(),
     tailwindcss()
